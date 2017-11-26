4e64560bdaa1388322355f365727e409249506f1
JeffBaucom <jeffbaucom@gmail.com>
2017-11-25 16:53:18 -0800

    who's that pokemon assignment

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 for (var i = 1; i <= 151; i++) {
-    $('.image-container').append($('<img>').attr('src', 'sprites/sprites/pokemon/' + i + '.png'))
+    $('.image-container').append($('<img>').attr('id', i).attr('src', 'sprites/sprites/pokemon/' + i + '.png'))
 }
+
+$('img').on('click', function(event) {
+	$('.poke-dex').html('');
+    var id = $(this).attr('id');
+    $.get("https://pokeapi.co/api/v2/pokemon/" + id + '/', function(data) {    
+        console.log(data);
+        var types = "<ul>";
+        for (var i = 0; i < data.types.length; i++) {
+        	types += "<li>" + data.types[i].type.name + "</li>";
+        }
+        types += "</ul>";
+        var info = "<h5>Types</h5>" + types + "<h5>Height</h5>" + data.height + "<h5>Weight</h5>" + data.weight; 
+        console.log(types);
+        data.name = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+        $('.poke-dex').append("<h2>" + data.name + "</h2>");
+        $('.poke-dex').append("<img src="+  'sprites/sprites/pokemon/' + data.id + '.png' + ">");
+        $('.poke-dex').append(info);
+    });
+    event.stopPropagation();
+});
